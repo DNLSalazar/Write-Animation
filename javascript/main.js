6f055579f82b5ea6ec4b2aa9a59e0316eada5351
Daniel Salazar <dnlsalazar27@gmail.com>
2019-09-02 22:26:41 -0400

    Ya casi está listo

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,34 @@
+var i = 0;
+var arrayWords = ["Ogre", "Windranger", "OD", "Pudge", "Mirana", "Invoker"];
+var wordElement = document.getElementById("word");
+var currentWord = wordElement.innerHTML;
+var status = 0;
+
 window.addEventListener("load", function () {
-  var arrayWords = ["Ogre", "Windranger", "OD", "Pudge", "Mirana", "Invoker"];
-  var wordElement = document.getElementById("word");
-  var currentWord = wordElement.innerHTML;
-  var status = false;
-  setInterval(animation, 150, wordElement, currentWord, arrayWords, status);
+  setInterval(animation, 200);
 });
 
 
-function animation (wordElement, currentWord, arrayWords, status) {
-  if (status);
+function animation () {
+
+  if (status == 0) {
+    if (currentWord.length < arrayWords[i].length) {
+      currentWord += arrayWords[i].slice(currentWord.length, currentWord.length+1);
+      wordElement.innerHTML = currentWord;
+    } else {
+      status = 1;
+      return;
+    }
+  } else {
+    if (currentWord.length > 0 && status == 1) {
+      currentWord = currentWord.slice(0, currentWord.length - 1);
+      wordElement.innerHTML = currentWord;
+    } else {
+      i++;
+      if(i == arrayWords.length) i = 0;
+      status = 0;
+      return;
+    }
+  }
+
 }
