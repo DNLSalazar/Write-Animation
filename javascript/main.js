3f074974d3558be6b6a7e63b402117a5dc4e0adb
Daniel Salazar <dnlsalazar27@gmail.com>
2019-09-02 21:49:54 -0400

    Working on automatic writing

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,12 @@
 window.addEventListener("load", function () {
-    alert("HI");
+  var arrayWords = ["Ogre", "Windranger", "OD", "Pudge", "Mirana", "Invoker"];
+  var wordElement = document.getElementById("word");
+  var currentWord = wordElement.innerHTML;
+  var status = false;
+  setInterval(animation, 150, wordElement, currentWord, arrayWords, status);
 });
+
+
+function animation (wordElement, currentWord, arrayWords, status) {
+  if (status);
+}
